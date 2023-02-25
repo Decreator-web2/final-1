4aaa93597e565408341ce1fcc2680a3d03d9d488
Decreator-web2 <122569583+Decreator-web2@users.noreply.github.com>
2023-02-26 01:02:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+string[] array = Array1();
+string[] result = Find(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] Find(string[] input, int n) {
+    string[] output = new string[Count(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+int Count(string[] input, int n) {
+    int count = 0;
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+    return count;
+}
+string[] Array1() {
+    Write("Введите значения : ");
+    return ReadLine().Split(" ");
+}
